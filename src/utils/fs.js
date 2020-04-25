27cfa789488a9f247bed9cf8bf8dccf917c52515
Joakim Hedlund <contact@joakimhedlund.com>
2020-04-25 17:35:04 +0200

    Initial fs utils

---
+++ src/utils/fs.js
@@ -0,0 +1,69 @@
+const fs = require('fs')
+const path = require('path')
+const { promisify } = require('util')
+
+exports.access = promisify(fs.access)
+exports.lstat = promisify(fs.lstat)
+exports.readDir = promisify(fs.readdir)
+exports.readFile = promisify(fs.readFile)
+exports.stat = promisify(fs.stat)
+exports.writeFile = promisify(fs.writeFile)
+
+/**
+ * Finds and returns the path to an upwards file by traversing parent directories
+ * until either the file exists or the directory is in the root of the filesystem.
+ *
+ * @param filename Name of file to look for
+ * @param directory (Optional) Directory to start looking in.
+ * @returns {string|false} Absolute path to the file.
+ */
+exports.findUpwardsFile = async (filename, directory = process.cwd()) => {
+  const parsedPath = path.parse(path.join(directory, filename))
+  const targetFile = `${parsedPath.dir}${parsedPath.base}`
+  let fileExists = false
+  try {
+    await exports.access(targetFile, 'utf8')
+    fileExists = true
+  } catch (err) {
+    if (err.code !== 'ENOENT') throw err
+  }
+  if (fileExists) {
+    // yay!
+    return targetFile
+  } else {
+    if (parsedPath.dir === parsedPath.root) {
+      // We're at the root of the filesystem. There's nowhere else to look.
+      return false
+    } else {
+      // Keep digging
+      return exports.findUpwardsFile(filename, path.dirname(directory))
+    }
+  }
+}
+
+/**
+ * Retrieve all filenames in a directory, excluding subdirectories.
+ *
+ * @param directoryPath
+ * @return {string[]} Filenames
+ */
+exports.listDirectoryFiles = async (directoryPath) => {
+  // Note: withFileTypes requires Node 10+
+  const dir = await exports.readDir(directoryPath, { withFileTypes: true })
+
+  // Backwards compatibility
+  const dirItems = await Promise.all(
+    dir.map(async (item) => {
+      // node 10.10+
+      if (item.isDirectory) return item
+      // Node 8 mostly
+      const fsStatsObject = await exports.lstat(path.join(directoryPath, item))
+      fsStatsObject.name = item
+      return fsStatsObject
+    })
+  )
+
+  return dirItems
+    .filter(item => !item.isDirectory())
+    .map(item => item.name)
+}
---
+++ src/utils/fs.spec.js
@@ -0,0 +1,37 @@
+
+describe('access()', () => {
+  it.todo('Calls fs.access')
+})
+
+describe('lstat()', () => {
+  it.todo('Calls fs.lstat')
+})
+
+describe('readDir()', () => {
+  it.todo('Calls fs.readdir')
+})
+
+describe('readFile()', () => {
+  it.todo('Calls fs.readFile')
+})
+
+describe('stat()', () => {
+  it.todo('Calls fs.stat')
+})
+
+describe('writeFile()', () => {
+  it.todo('Calls fs.writeFile')
+})
+
+describe('findUpwardsFile()', () => {
+  it.todo('looks for file in the supplied directory')
+  it.todo('defaults to process.cwd()')
+  it.todo('returns an absolute path when matching file is found')
+  it.todo('returns false when file cannot be found')
+  it.todo('traverses the directory tree')
+  it.todo('???')
+})
+describe('listDirectoryFiles()', () => {
+  it.todo('lists files in the supplied directory')
+  it.todo('???')
+})
