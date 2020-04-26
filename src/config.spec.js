5cdcecbac1ce4e107b43fec19523d539a483742d
Joakim Hedlund <contact@joakimhedlund.com>
2020-04-26 12:40:52 +0200

    Outline tests

---
+++ src/config.spec.js
@@ -0,0 +1,43 @@
+
+describe('getSampleConfig()', () => {
+  it.todo('Calls fs.access')
+})
+
+describe('getConfig()', () => {
+  it.todo('finds config in current or parent directories')
+  it.todo('merges with default settings')
+
+  describe('config.migrationsDirectory', () => {
+    it.todo('points to an absolute path')
+  })
+
+  describe('config.context()', () => {
+    it.todo('returns a singleton')
+  })
+
+  describe('config.fetchState()', () => {
+    it.todo('falls back to file-based state storage')
+    it.todo('returns a singleton')
+  })
+
+  describe('config.storeState()', () => {
+    it.todo('test')
+    it.todo('falls back to file-based state storage')
+  })
+
+  describe('config.beforeAll()', () => {
+    it.todo('test')
+  })
+
+  describe('config.beforeEach()', () => {
+    it.todo('test')
+  })
+
+  describe('config.afterEach()', () => {
+    it.todo('test')
+  })
+
+  describe('config.afterAll()', () => {
+    it.todo('test')
+  })
+})
---
+++ src/migrations.spec.js
@@ -0,0 +1,12 @@
+
+describe('getSampleMigration()', () => {
+  it.todo('test')
+})
+
+describe('getPendingJobs()', () => {
+  it.todo('test')
+})
+
+describe('up()', () => {
+  it.todo('test')
+})
