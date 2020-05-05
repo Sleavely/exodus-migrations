4f2d1462381da9ffcf94769e5643fabec5b2dfbb
amoj <amoj@users.noreply.github.com>
2020-05-05 23:30:06 +0200

    Update fs.spec.js

--- src/utils/fs.spec.js
+++ src/utils/fs.spec.js
@@ -24,7 +24,7 @@ describe('lstat()', () => {
   })
 })
 
-describe('mkdir()', () => {
+describe('mkdir()', async () => {
   it('Calls fs.mkdir', () => {
     const { mkdir } = jest.requireActual('./fs')
 
